a26159f3073786621e968b8484f05243f8104a37
Mohit-Gyansys <doramohitkumar@gmail.com>
2025-06-03 17:00:13 +0530

    initial commit

---
+++ prisma/migrations/20250603112243_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "DemoTable" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "DemoTable_pkey" PRIMARY KEY ("id")
+);
