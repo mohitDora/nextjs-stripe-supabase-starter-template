65b3e1531785a6cbd7789f3fa8231955ac861917
Mohit-Gyansys <doramohitkumar@gmail.com>
2025-06-05 12:19:10 +0530

    verify page updated email removed

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 
-export default function Verify({
-  searchParams,
-}: {
+interface VerifyPageProps {
   searchParams: { email?: string };
-}) {
-  const email = searchParams.email;
+}
+
+export default function Verify() {
+  // const email = searchParams.email;
 
   return (
     <>
-      <div>Verify {email}</div>
+      {/* <div>Verify {email}</div> */}
       <Link href="/" className="text-blue-600 underline">
         change email
       </Link>
