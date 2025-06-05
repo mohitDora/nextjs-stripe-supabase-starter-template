a6ff4262b75e344c46a19557ed248c0ecef42c63
Mohit-Gyansys <doramohitkumar@gmail.com>
2025-06-05 12:58:08 +0530

    verify page updated email removed 3

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 
-import { Button } from '@/components/ui/button'
-import React from 'react'
-import { supabase } from '@/config/supabse/client'
-import { useRouter } from 'next/navigation'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { supabase } from "@/config/supabse/client";
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
   const router = useRouter();
-  const logout = async() => {
-      await supabase.auth.signOut();
-      router.refresh()
-  }
+  const logout = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
   return (
     <>
-    <div>Dashboard</div>
-    <Button onClick={logout}>Logout</Button>
+      <div>Dashboard</div>
+      <Button onClick={logout}>Logout</Button>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
